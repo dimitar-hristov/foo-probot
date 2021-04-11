1176135e47331bab2e37ae59afb80dffef08c022
dimitar-hristov <dimitarshr@gmail.com>
2021-04-11 16:46:14 +0100

    Convert to GH action

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+const { run } = require('@probot/adapter-github-actions')
+const app = require('./app')
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+run(app).catch((error) => {
+    console.error(error);
+    process.exit(1)
+})
